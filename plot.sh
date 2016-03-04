161b28cd89fc341b63058968a1493332b5cf2aa3
Kai Hendry <hendry@webconverger.com>
2016-03-04 09:04:48 +0800

    Attempt to make the plot look nicer. By default gnuplot seems to clip the right edge of the xlabels.

--- plot.sh
+++ plot.sh
@@ -11,9 +11,15 @@ cat "$1"
 echo EOD
 
 cat << END
-set term svg size 800,600 fname "Helvetica Neue" fsize 9 rounded dashed
-set title "$1"
+set term svg size 1024,768
+set title "$1 https://github.com/kaihendry/graphsloc"
 set xdata time
+set xtics rotate by 45 right
 set timefmt "%s"
+set format y '%.0s %c'
+set ylabel "Source lines of code"
+unset key
+set tic scale 0
+set grid ytics
 plot \$data u 1:2 w lines
 END
