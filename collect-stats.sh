f129e8d2aa32cc0f603feafd2e3f2a3b42a389be
Kai Hendry <hendry@webconverger.com>
2016-03-06 16:12:17 +0800

    Appears to work

--- collect-stats.sh
+++ collect-stats.sh
@@ -8,60 +8,44 @@ fi
 
 gitrepo=$1
 
-checkmaster() {
-	git --git-dir=$gitrepo/.git --work-tree $gitrepo checkout master
-}
-
-trap "checkmaster" EXIT
-
-gwc() { # fast (though can you make this faster?)
-	git ls-tree --name-only -r HEAD -z | wc  -l --files0-from=- 2>/dev/null | tail -n1 | awk '{ print $1 }'
-}
-
-sloc() { # slow
-	# https://github.com/flosse/sloc
-	sloc -f json . | jq .summary.total
-}
-
-# Author : Teddy Skarin
 # https://github.com/fearside/ProgressBar/blob/master/progressbar.sh
 ProgressBar() {
-# Process data
-_progress=$(((${1}*100/${2}*100)/100))
+_progress=$((($1*100/$2*100)/100))
 _done=$((_progress*4/10))
 _left=$((40-_done))
 # Build progressbar string lengths
 _done=$(printf "%${_done}s")
 _left=$(printf "%${_left}s")
-
-# 1.2 Build progressbar strings and print the ProgressBar line
-# 1.2.1 Output example:
-# 1.2.1.1 Progress : [########################################] 100%
 printf "\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
 }
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$gitrepo" || exit
 
-git pull
-checkmaster
 fn=$DIR/$(basename "$(pwd)")-$(git describe --always).csv
 test -s "$fn" && exit
 
 mapfile -t revs < <(git rev-list master)
 len=${#revs[@]}
-inc=1
-test "$SAMPLE" && inc=$(($len / $SAMPLE)) && echo Incrementing by $inc
-llen=$(($len - $inc))
 echo $len git commits
+total=0
 
-for (( i=0; i < $len; i+=$inc ));
+for (( i=$len; i > 0; i-- ));
 do
-	git checkout -q "${revs[$i]}"
-	echo "$(git show -s --format="%ct" .)" "$(gwc)" >> "$fn"
-	ProgressBar $i $llen
-done
-echo -e "\n"
 
-echo ./plot.sh "$fn" \| gnuplot \> "$fn.svg"
+	#echo git show --numstat --format="%ct" ${revs[$i]}
+	#foo=$(git show --numstat --format="%ct" ${revs[$i]} |awk 'NR==1{print;next} NF==3 {plus+=$1; minus+=$2} END {printf(plus-minus)}')
+	#set -- $foo
+
+	set -- $(git show --numstat --format="%ct" ${revs[$i]} |awk 'NR==1{print;next} NF==3 {plus+=$1; minus+=$2} END {printf(plus-minus)}')
+	time=$1
+	count=$2
+	#echo $time $count
+	total=$((total + count))
+	#echo Total $total
+	echo $time $total >> $fn
+	#ProgressBar $(($len-$i)) $len
+done
 cd "$DIR"
+
+echo -e "\n ./plot.sh $fn | gnuplot > $fn.svg"
